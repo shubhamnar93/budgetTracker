41da2823a4dec1ec9a90855f4bd04c213ccc4ce0
shubhamnar93 <shubhamnarnolia925@gmail.com>
2025-08-20 13:37:41 +0530

    adding navbar

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { auth } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <div></div>
+    </HydrateClient>
+  );
+}
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-
 import { db } from "@/server/db";
+import Google from "next-auth/providers/google";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -32,7 +31,17 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
     /**
      * ...add more providers here.
      *
