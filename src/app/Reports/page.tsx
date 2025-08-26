adae55c192b83489306cdd5390a2d4c3b513a6bb
shubhamnar93 <shubhamnarnolia925@gmail.com>
2025-08-26 11:26:03 +0530

    feat: adding suspanse to constrols

--- src/app/Reports/page.tsx
+++ src/app/Reports/page.tsx
@@ -1,10 +1,8 @@
-import { Controls } from "./controls";
 import { Reports } from "./reports";
 
 export default function ReportsPage() {
   return (
     <div className="mx-auto max-w-6xl px-6 py-8">
-      <Controls />
       <Reports />
     </div>
   );
--- src/app/Reports/reports.tsx
+++ src/app/Reports/reports.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from "react";
 import { ViewType } from "./viewType";
+import { Controls } from "./controls";
 
 export const Reports = () => {
   return (
     <Suspense>
+      <Controls />
       <ViewType />
     </Suspense>
   );
